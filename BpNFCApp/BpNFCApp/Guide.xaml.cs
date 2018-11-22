ff8aefcba9d7fcc2a1845d37559c5b563b54a9ab
Ida Skovbjerg <ida_skovbjerg@hotmail.com>
2018-11-22 12:02:58 +0100

    5 minutes Timer

---
+++ BpNFCApp/BpNFCApp/Guide.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace BpNFCApp
+{
+    /// <summary>
+    /// Interaction logic for Guide.xaml
+    /// </summary>
+    public partial class Guide : Window
+    {
+        private DispatcherTimer _timer;
+        private TimeSpan _time;
+        public Guide()
+        {
+            InitializeComponent();
+            _time = TimeSpan.FromMinutes(5);
+
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                TimerTextBlock.Text = _time.ToString("c");
+                if (_time == TimeSpan.Zero) _timer.Stop();
+                _time = _time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+
+            _timer.Start();
+
+        }
+
+    }
+}
